243a8642a18b777d2f5f28d309b7107470a4f845
hamraskannur <kkhamras741@gmail.com>
2023-03-16 22:26:33 +0530

    ui completed

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Dashboards from "./Dashboards";
 import { leftBarMenus ,leftBarProjects } from "../../data"
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react'
-import NavBar from '../components/NavBar/NavBar'
-import Section from '../components/section/Section'
-import SideBar from '../components/leftBar/LeftBar'
-import TaskBoards from '../components/TaskBoar/TaskBoards'
-import RightBar from '../components/rightBar/RightBar'
-import { BacklogTasks,Done,Inprogress,ToDoTasks } from "../data";
+import React, { lazy, Suspense } from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import SideBar from '../components/leftBar/LeftBar';
+import TaskBoards from '../components/TaskBoar/TaskBoards';
+import RightBar from '../components/rightBar/RightBar';
+import { BacklogTasks, Done, Inprogress, ToDoTasks } from '../data';
+
+const LazySection = lazy(() => import('../components/section/Section'));
 
 export default function Home() {
   
+
   return (
     <>
-  
-    <div className='flex flex-col lg:flex-row bg-[#F7F8FA] min-h-screen"'>
-      <SideBar />
-      <div className='flex-1 false lg:block ' style={{transform: "translateX(0vw) translateZ(0px)"}}>
-         <NavBar/>
-         <TaskBoards/>
+      <div className='flex flex-col lg:flex-row bg-[#F7F8FA] min-h-screen'>
+        <SideBar />
+        <div className='flex-1 false lg:block'>
+          <NavBar />
+          <TaskBoards />
           <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 px-[16px] md:px-[36px] pb-[28px] gap-[24px]'>
-            <Section task={BacklogTasks} taskName={"Backlog Tasks"}/>
-            <Section task={ToDoTasks} taskName={"To Do Tasks"}/>
-            <Section task={Inprogress} taskName={"In Progress"}/>
-            <Section task={Done} taskName={"Done"}/>
+            <Suspense fallback={<div>Loading...</div>}>
+              <LazySection task={BacklogTasks} taskName={'Backlog Tasks'} />
+              <LazySection task={ToDoTasks} taskName={'To Do Tasks'} />
+              <LazySection task={Inprogress} taskName={'In Progress'} />
+              <LazySection task={Done} taskName={'Done'} />
+            </Suspense>
           </div>
-      </div> 
-     <RightBar/>
-    </div>
+        </div>
+        <RightBar />
+      </div>
     </>
-  )
+  );
 }
